3e42873b563e9a096b281cc43d6f6e744787f436
dang <107794701+DanGavrielov@users.noreply.github.com>
2024-12-03 11:42:15 +0200

    minor refactor

--- src/day_03/Day03.kt
+++ src/day_03/Day03.kt
@@ -10,44 +10,25 @@ fun main() {
 
     fun part1(input: List<String>): Int {
         val regex = Regex("mul\\([0-9]{1,3},[0-9]{1,3}\\)")
-        var result = 0
-        input.forEach { line ->
-            val matches = regex.findAll(line).map { it.value }.toList()
-            matches.forEach { mulCommand ->
-                val digits = mulCommand.drop(4).dropLast(1).split(",").map { it.toInt() }
-                result += digits.reduce { acc, i ->
-                    return@reduce acc * i
-                }
-            }
-        }
-        return result
+        val matches = extractMatches(input, regex)
+        return calculateProduct(matches)
     }
 
     fun part2(input: List<String>): Int {
         val regex = Regex("mul\\([0-9]{1,3},[0-9]{1,3}\\)|do\\(\\)|don't\\(\\)")
-        var result = 0
         var collecting = true
-        input.forEach { line ->
-            val matches = regex.findAll(line).map { it.value }.toList()
+        val filteredMatches = mutableListOf<String>()
 
-            val filteredMatches = mutableListOf<String>()
-            matches.forEach { match ->
-                when (match) {
-                    "don't()" -> collecting = false
-                    "do()" -> collecting = true
-                    else -> if (collecting) filteredMatches.add(match)
-                }
-            }
-
-            filteredMatches.forEach { mulCommand ->
-                val digits = mulCommand.drop(4).dropLast(1).split(",").map { it.toInt() }
-                result += digits.reduce { acc, i ->
-                    return@reduce acc * i
-                }
+        val matches = extractMatches(input, regex)
+        matches.forEach { match ->
+            when (match) {
+                "don't()" -> collecting = false
+                "do()" -> collecting = true
+                else -> if (collecting) filteredMatches.add(match)
             }
         }
 
-        return result
+        return calculateProduct(filteredMatches)
     }
 
     // Test if implementation meets criteria from the description, like:
@@ -58,4 +39,17 @@ fun main() {
     val input = readInput(dir = "day_03", name = "Day03")
     part1(input).println()
     part2(input).println()
+}
+
+private fun extractMatches(input: List<String>, regex: Regex): List<String> {
+    return input.flatMap { line ->
+        regex.findAll(line).map { it.value }.toList()
+    }
+}
+
+private fun calculateProduct(matches: List<String>): Int {
+    return matches.sumOf { mulCommand ->
+        val digits = mulCommand.drop(4).dropLast(1).split(",").map { it.toInt() }
+        digits.reduce { acc, i -> acc * i }
+    }
 }
